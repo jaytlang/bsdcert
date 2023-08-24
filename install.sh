2116d11eb2a36e4c84523f211b67006be7a30e93
Jay Lang <jaytlang@iesc-s3.mit.edu>
2023-08-24 15:50:32 -0400

    make this work with relayd

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cat << EOF
+====
 Hi! You should run this script instead of section A.2
 of the thesis. It will set up certificates automatically
 for you, including their renewal, which is very convenient.
@@ -26,12 +27,47 @@ pass in on egress proto tcp from any to egress port {ssh, http, https}
 
 Just make sure 'http' is in there. Press any key to acknowledge
 this caveat, and we will get on with things.
+====
 EOF
 read
 
 # configure everything
 doas sh -c 'sed -e "s/CHANGEME/`hostname`/" acme-client.conf > /etc/acme-client.conf'
 doas sh -c 'sed -e "s/CHANGEME/`hostname`/" httpd.conf > /etc/httpd.conf'
+
+rcctl check relayd
+if [ $? -eq 0 ]; then
+	cat << EOF
+====
+I noticed relayd is running. We should configure ssh
+public key authentication for the worker machine fpga
+account so that we can automatically update all worker
+certificates. I will install ssh-copy-id to make this
+happen, and then uninstall it once public key authentication
+is configured.
+
+If at any point in the future you want to add more
+workers, come back here and rerun this installer script.
+We will take care of setting public keys for you at that
+point.
+
+Please set up an ssh identity for _root_ in /root/.ssh.
+Press Ctrl+C if you haven't done that already. Otherwise,
+press any other key to continue.
+====
+EOF
+	read
+
+	doas pkg_add ssh-copy-id
+
+	hosts=`doas relayctl show hosts | grep .mit.edu | cut -f 3`
+	for host in $hosts; do
+		doas ssh-copy-id fpga@$host
+		[ $? -eq 0 ] || exit 1
+	done
+
+	doas pkg_delete ssh-copy-id
+fi
 
 # go
 doas install -o root -g wheel -m 555 renewcerts /usr/local/bin
